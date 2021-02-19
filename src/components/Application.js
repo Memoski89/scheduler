9007475977596c4ee5c6dafe980025c47b35ca89
Louis <l.g.coreas@gmail.com>
2021-02-19 15:53:51 -0500

    Implement the getInterview function to pass the two tests

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import "components/Appointment";
 import Appointment from "components/Appointment";
-import getAppointmentsForDay from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   /*   const [days, setDays] = useState([]);
@@ -13,26 +13,39 @@ export default function Application(props) {
     day: "Monday",
     days: [],
     appointments: {},
+    interviewers: {},
   });
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
   const setDay = (day) => setState({ ...state, day });
 
   useEffect(() => {
-    const dayURL = `http://localhost:8001/api/days`;
-    const ApptURL = `http://localhost:8001/api/appointments`;
-    Promise.all([axios.get(dayURL), axios.get(ApptURL)]).then((all) => {
-      console.log(all);
-      setState((prev) => ({
-        ...prev,
-        days: all[0].data,
-        appointments: all[1].data,
-      }));
+    const dayURL = `/api/days`;
+    const apptURL = `/api/appointments`;
+    const intURL = `/api/interviewers`;
+
+    Promise.all([
+      axios.get(dayURL),
+      axios.get(apptURL),
+      axios.get(intURL),
+    ]).then((all) => {
+      const days = all[0].data;
+      const appointments = all[1].data;
+      const interviewers = all[2].data;
+      setState((prev) => ({ ...prev, days, appointments, interviewers }));
     });
   }, []);
 
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
   const appointments = dailyAppointments.map((appointment) => {
-    return <Appointment key={appointment.id} {...appointment} />;
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+        interview={interview}
+      />
+    );
   });
+
   return (
     <main className="layout">
       <section className="sidebar">
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,14 +1,26 @@
-export default function getAppointmentsForDay(state, dayname) {
+export function getAppointmentsForDay(state, dayname) {
+  const result = [];
+
+  const dayObj = state.days.find((d) => d.name === dayname);
+  if (!dayObj) {
+    return result;
+  }
+
+  for (let id of dayObj.appointments) {
+    result.push(state.appointments[id]);
+  }
+  return result;
+
   //coding golf it's too robotic too clever
-  return (
-    state.days
-      .filter((day) => day.name === dayname)
-      .map((day) =>
-        day.appointments.map(
-          (appointmentid) => state.appointments[appointmentid]
-        )
-      )[0] || []
-  );
+  // return (
+  //   state.days
+  //     .filter((day) => day.name === dayname)
+  //     .map((day) =>
+  //       day.appointments.map(
+  //         (appointmentid) => state.appointments[appointmentid]
+  //       )
+  //     )[0] || []
+  // );
   // console.log(dayname, item[0]);
   // return item[0] || [];
 
@@ -21,4 +33,30 @@ export default function getAppointmentsForDay(state, dayname) {
   //   );
   // }
   // return filtereddays;
+}
+
+export function getInterview(state, interview) {
+  //  return a new object containing the interview data when we pass it an object that contains the interviewer.
+  if (!interview) {
+    return null;
+  }
+
+  const interviewerId = interview.interviewer;
+  const interviewer = state.interviewers[interviewerId];
+  const result = { ...interview, interviewer };
+  console.log(result);
+  return result;
+
+  // return (
+  //   state.appointments
+  //     .filter(
+  //       (appointmentid) =>
+  //         state.appointmentid[appointmentid].interview === interview
+  //     )
+  //     .map((appointmentid) =>
+  //       appointmentid.interview.interviewer.map(
+  //         (interviewerid) => state.interviewers[interviewerid]
+  //       )
+  //     )[0] || null
+  // );
 }
